b9ee2bc9ebedd5bf55912ea38b883f72b4e077cc
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-06 02:45:30 -0300

    Layout fineshed

---
+++ app/src/main/java/com/rafael/simulator/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.rafael.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rafael.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
